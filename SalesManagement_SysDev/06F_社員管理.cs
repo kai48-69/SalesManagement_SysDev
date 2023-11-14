7a4427a47d5b74ec0b9689efe968eb64daf565d9
saior <C2130@oic.jp>
2023-11-14 17:36:53 +0900

    ko

--- SalesManagement_SysDev/06F_社員管理.cs
+++ SalesManagement_SysDev/06F_社員管理.cs
@@ -18,6 +18,7 @@ namespace SalesManagement_SysDev
             InitializeComponent();
         }
 
+        private InputCheck ichk = new InputCheck();
         EmployeeDbConnection DB = new EmployeeDbConnection();
         private static List<M_SalesOffice> SoNameDsp;
         private static List<M_Position> PoNameDsp;
@@ -43,7 +44,7 @@ namespace SalesManagement_SysDev
         private bool GetDataGridView()
         {
             //在庫情報の全件取得
-            List<DispProductListDTO> tb = DB.ProductGetData("", "", 0);
+            List<DispEmployeeListDTO> tb = DB.EmployeeGetData("",0);
             if (tb == null)
                 return false;
             //データグリッドビューへの設定
@@ -72,6 +73,122 @@ namespace SalesManagement_SysDev
             //読み込み専用に
             ComboEigyousyoName.DropDownStyle = ComboBoxStyle.DropDownList;
             ComboYakusyokuName.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void SetDataGridView(List<DispEmployeeListDTO> tb)
+        {
+            dataGridView1.DataSource = tb;
+            //列幅自動設定解除
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            //ヘッダーの高さ
+            dataGridView1.ColumnHeadersHeight = 50;
+            //ヘッダーの折り返し表示
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            //行単位選択     
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            //ヘッダー文字位置、セル文字位置、列幅の設定
+            ////商品ID
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[0].Width = 40;
+            ////メーカー名
+            dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[1].Width = 50;
+            //商品名
+            dataGridView1.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[2].Width = 80;
+            //価格
+            dataGridView1.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[3].Width = 80;
+            ////安全在庫数
+            dataGridView1.Columns[4].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[4].Width = 40;
+            ////小分類
+            dataGridView1.Columns[5].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[5].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[5].Width = 80;
+            ////型番
+            dataGridView1.Columns[6].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[6].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[6].Width = 30;
+            ////色
+            dataGridView1.Columns[7].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[7].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[7].Width = 70;
+            ////発売日
+            dataGridView1.Columns[8].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[8].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[8].Width = 80;
+            ////非表示理由 
+            dataGridView1.Columns[9].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[9].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[9].Width = 400;
+
+            dataGridView1.Refresh();
+        }
+
+        private void ButtonExe_Click(object sender, EventArgs e)
+        {
+            if (RadioTouroku.Checked == true)
+            {
+                if (!GetVaildDataAtRegistration())
+                {
+                    return;
+                }
+                var regPro = GenerateDataAtRegistration();
+
+                RegistrationProduct(regPro);
+            }
+        }
+
+        private bool GetVaildDataAtRegistration() //入力データチェック
+        {
+            if (String.IsNullOrEmpty(TextboxSyainID.Text.Trim()))
+            {
+                if (!ichk.IntegerCheck(TextboxSyainID.Text.Trim()))
+                {
+                    MessageBox.Show("安全在庫数は半角数字で入力してください");
+                    TextboxStock.Focus();
+                    return false;
+                }
+            }
+
+            if (!String.IsNullOrEmpty(TextboxStock.Text.Trim()))
+            {
+                if (!ichk.IntegerCheck(TextboxStock.Text.Trim()))
+                {
+                    MessageBox.Show("安全在庫数は半角数字で入力してください");
+                    TextboxStock.Focus();
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("安全在庫数が入力されていません");
+                TextboxStock.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private M_Employee GenerateDataAtRegistration() //登録データ生成
+        {
+            string SoID = ComboEigyousyoName.SelectedIndex.ToString();
+            string PoID = ComboYakusyokuName.SelectedValue.ToString();
+            return new M_Employee
+            {
+                EmID = int.Parse(TextboxSyainID.Text.Trim()),
+                EmName = TextboxSyainName.Text.Trim(),
+                PoID = int.Parse(PoID),
+                EmPhone = int.Parse(TextboxTelNo.Text.Trim(),
+                EmFlag
+                EmHidden = null,
+            };
         }
     }
 }
