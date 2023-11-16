6b3b30805974920ce40bb4cabf61270a7d01239d
C2181 <C2181@oic.jp>
2023-11-16 16:25:11 +0900

    更新

--- SalesManagement_SysDev/ClientDataAccess.cs
+++ SalesManagement_SysDev/ClientDataAccess.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalesManagement_SysDev
 {
-    internal class ClientDateAccess
+    internal class ClientDataAccess
     {
+        public bool AddClientData(M_Client regCl)
+        {
+            try
+            {
+                var context = new SalesManagement_DevContext();
+                context.M_Clients.Add(regCl);
+                context.SaveChanges();
+                context.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "例外エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
     }
 }
