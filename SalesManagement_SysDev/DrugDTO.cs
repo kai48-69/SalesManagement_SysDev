05cae7ded97486462e86db01820252b3f45cd9d3
C2181 <C2181@oic.jp>
2023-12-01 16:51:55 +0900

    更新

--- SalesManagement_SysDev/DrugDTO.cs
+++ SalesManagement_SysDev/DrugDTO.cs
@@ -133,5 +133,20 @@ namespace SalesManagement_SysDev
         public string SoName { get; set; }
         public int PoID { get; set; }
     }
-}
+
+    class DispWareHousingListDTO
+    {
+        [DisplayName("入庫ID")]
+        public int WaID { get; set; }
+        [DisplayName("入庫詳細ID")]
+        public int WaDetailID { get; set; }
+        [DisplayName("発注ID")]
+        public int HaID { get; set;}
+        [DisplayName("商品名")]
+        public string PrName { get; set; }
+        [DisplayName("数量")]
+        public int WaQuantity { get; set; }
+        
+    }
+    }
 
